4936a9ff6735f2fa8b7f9634aa2be3aba5c057ef
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-28 15:27:59 +0200

    feat: create components basics

--- clinet/src/App.js
+++ clinet/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Typography, AppBar} from '@material-ui/core'
+
 
 function App() {
     return (
         <div>
-            Video Chat App
+            <AppBar 
+                position="static" 
+                color="inherit">
+                    <Typography variant="h2" align="center">
+                        Vide Chat App
+                    </Typography>
+            </AppBar>
+            {/* Video Player */}
+            {/* Options => Notifications */}
         </div>
     )
 }
