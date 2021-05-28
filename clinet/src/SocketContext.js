4a399814484fec44a7e1fa709fd663548159d30f
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-28 20:19:19 +0200

    fix: error correction

--- clinet/src/SocketContext.js
+++ clinet/src/SocketContext.js
@@ -8,103 +8,101 @@ const locaHostUrl = 'http://localhost:5000'
 
 const socket = io(locaHostUrl)
 
-const ContextProvider = ({children}) =>{
 
-    const [stream, setStream] = useState(null)
-    const [me, setMe] = useState('')
-    const [name, setName] = useState('')
-    const [call, setCall] = useState({})
-    const [callAccepted, setCallAccepted] = useState(false)
-    const [callEnded, setCallEnded] = useState(false)
+const ContextProvider = ({ children }) => {
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+    const [stream, setStream] = useState();
+    const [name, setName] = useState('');
+    const [call, setCall] = useState({});
+    const [me, setMe] = useState('');
+  
+    const myVideo = useRef();
+    const userVideo = useRef();
+    const connectionRef = useRef();
+  
+    useEffect(() => {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then((currentStream) => {
+          setStream(currentStream);
+  
+          myVideo.current.srcObject = currentStream;
+        });
+  
+      socket.on('me', (id) => setMe(id));
+  
+      socket.on('callUser', ({ from, name: callerName, signal }) => {
+        setCall({ isReceivingCall: true, from, name: callerName, signal });
+      });
+    }, []);
+  
+    const answerCall = () => {
+      setCallAccepted(true);
+  
+      const peer = new Peer({ initiator: false, trickle: false, stream });
+  
+      peer.on('signal', (data) => {
+        socket.emit('answerCall', { signal: data, to: call.from });
+      });
+  
+      peer.on('stream', (currentStream) => {
+        userVideo.current.srcObject = currentStream;
+      });
+  
+      peer.signal(call.signal);
+  
+      connectionRef.current = peer;
+    };
+  
+    const callUser = (id) => {
+      const peer = new Peer({ initiator: true, trickle: false, stream });
+  
+      peer.on('signal', (data) => {
+        socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
+      });
+  
+      peer.on('stream', (currentStream) => {
+        userVideo.current.srcObject = currentStream;
+      });
+  
+      socket.on('callAccepted', (signal) => {
+        setCallAccepted(true);
+  
+        peer.signal(signal);
+      });
+  
+      connectionRef.current = peer;
+    };
+  
+    const leaveCall = () => {
+      setCallEnded(true);
+  
+      connectionRef.current.destroy();
+  
+      window.location.reload();
+    };
+  
+    return (
+      <SocketContext.Provider value={{
+        call,
+        callAccepted,
+        myVideo,
+        userVideo,
+        stream,
+        name,
+        setName,
+        callEnded,
+        me,
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+      >
+        {children}
+      </SocketContext.Provider>
+    );
+  };
+  
+  export { ContextProvider, SocketContext };
 
-    const myVideo = useRef()
-    const userVideo = useRef()
-    const connectionRef = useRef()
 
-    useEffect(() =>{
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        })
-        .then((currentStream) =>{
-            setStream(currentStream)
-            myVideo.current.srcObject = currentStream
-        })
-        socket.on('me', (id) => setMe(id))
-
-        socket.on('calluser', ({from, name: callerName, signal}) => setCall({
-            isReceivedCall: true,
-            from, 
-            name:callerName, 
-            signal
-        }))
-    }, [])
-
-    const answerCall = () =>{
-        setCallAccepted(true)
-
-        const peer = new Peer({
-            initiator:false,
-            trickle:false,
-            stream
-        })
-        peer.on('signal', (data) => socket.emit('answercall', {signal: data, to: call.from}))
-
-        peer.on('stream', (currentStream) =>{ userVideo.current.srcObject = currentStream })
-
-        peer.signal(call.signal)
-
-        connectionRef.current = peer
-
-    }
-
-    const callUser = (id) =>{
-
-        const peer = new Peer({
-            initiator:true,
-            trickle:false,
-            stream
-        })
-
-        peer.on('signal', (data) => socket.emit('calluser', {userToCall: id, signaData: data, from: me, name}))
-
-        peer.on('stream', (currentStream) =>{ userVideo.current.srcObject = currentStream })
-
-        peer.on('callaccepted', (signal) =>{ 
-
-            setCallAccepted(true)
-
-            peer.signal(signal)
-
-            connectionRef.current = peer
-         })
-    }
-    const leaveCall = () =>{
-
-        setCallEnded(true)
-
-        connectionRef.current.destroy()
-
-        window.location.reload()
-    }
-
-    return(
-        <SocketContext.Provider value={{
-            call,
-            callAccepted,
-            myVideo,
-            userVideo,
-            stream,
-            name,
-            setName,
-            callEnded,
-            me,
-            callUser,
-            answerCall,
-            leaveCall
-        }}> 
-            {children}
-        </SocketContext.Provider>
-    )
-}
-export { ContextProvider,  SocketContext}
--- clinet/src/components/Notifications.jsx
+++ clinet/src/components/Notifications.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React , {useContext} from 'react'
+import {Button } from '@material-ui/core'
+import {SocketContext} from '../SocketContext'
 
 const Notifications = () => {
+
+    const {
+        call,
+        callAccepted,
+        answerCall
+    } = useContext(SocketContext)
+
     return (
-        <div>
-            Notifications
-        </div>
+        
+        <>
+            { call.isReceivingCall && !callAccepted && (
+                <div style={{ display: 'flex', justifyContent: 'center'}}>
+                    <h1>{call.name} is calling</h1>
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        onClick={answerCall}
+                    >
+                        Answer Call
+                    </Button>
+                </div>
+            )}
+        </>
     )
 }
 
